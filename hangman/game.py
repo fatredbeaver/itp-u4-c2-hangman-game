9bc74ed6da56731665f95db713e2273a8b343d65
fatredbeaver <tedmundho@gmail.com>
2019-11-21 10:16:43 +0800

    Add files via upload

--- hangman/game.py
+++ hangman/game.py
@@ -1,23 +1,63 @@
 from .exceptions import *
-
+import random
 # Complete with your own, just for fun :)
-LIST_OF_WORDS = []
+LIST_OF_WORDS = ['LOL', 'HELLO', 'FOOL']
 
 
 def _get_random_word(list_of_words):
-    pass
+    if not list_of_words:
+        raise InvalidListOfWordsException
+    return random.choice(list_of_words)
 
 
 def _mask_word(word):
-    pass
+    if not word:
+        raise InvalidWordException
+    return '*' * len(word)
 
 
 def _uncover_word(answer_word, masked_word, character):
-    pass
+    if len(character) > 1:
+        raise InvalidGuessedLetterException
+    if not answer_word or not masked_word or len(answer_word) != len(masked_word):
+        raise InvalidWordException
+    character = character.lower()
+    new_masked_word = masked_word.lower()
+    answer_word = answer_word.lower()
+    for index, alpha in enumerate(answer_word): #just in case of repeated letters, iterating over each letter in answer_word
+        if character == alpha:
+            new_masked_word = new_masked_word[:index] + character + new_masked_word[(index + 1):]
+    return new_masked_word
+
 
 
 def guess_letter(game, letter):
-    pass
+    if letter in game['previous_guesses']:
+        raise InvalidGuessedLetterException
+
+    if game['answer_word'].lower() == game['masked_word'].lower() or game['remaining_misses'] == 0:
+        raise GameFinishedException
+
+    letter = letter.lower()
+    previous_masked = game['masked_word']
+    new_masked = _uncover_word(game['answer_word'], previous_masked, letter)
+
+    #letter not in answer, masked_word remains unchanged
+    if previous_masked == new_masked:
+        game['remaining_misses'] -= 1 #remaining misses go down by one
+
+    #letter in answer, masked_word is updated
+    elif previous_masked != new_masked:
+        game['masked_word'] = new_masked
+        
+    game['previous_guesses'].append(letter)
+
+    if game['answer_word'].lower() == game['masked_word'].lower():
+        raise GameWonException
+
+    if game['remaining_misses'] <= 0:
+        game['masked_word'] = _mask_word(game['masked_word'])
+        raise GameLostException
 
 
 def start_new_game(list_of_words=None, number_of_guesses=5):
