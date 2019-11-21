05cb3573ebb8b40fafebaa32e51a4d50d57483ff
fatredbeaver <tedmundho@gmail.com>
2019-11-21 09:07:38 +0800

    Add files via upload

--- game.py
+++ game.py
@@ -17,34 +17,39 @@ def _mask_word(word):
 
 
 def _uncover_word(answer_word, masked_word, character):
-    if not answer_word or not masked_word:
-        raise InvalidWordException
     if len(character) > 1:
         raise InvalidGuessedLetterException
-    if len(answer_word) != len(masked_word):
+    if not answer_word or not masked_word or len(answer_word) != len(masked_word):
         raise InvalidWordException
+    character = character.lower()
+    new_masked_word = masked_word.lower()
+    answer_word = answer_word.lower()
+    for alpha in answer_word: #just in case of repeated letters, iterating over each letter in answer_word
+        if character == alpha:
+            position_of_character = alpha.index(character)
+            new_masked_word[position_of_character] = character
+    return new_masked_word
 
-    answer = answer_word.lower()
-    if character.lower() in answer:
-        position = answer_word.index(character)
-        masked_word[position] = character
-    return masked_word
 
 
 def guess_letter(game, letter):
     letter = letter.lower()
-    if letter in game['previous_guesses']:
-        raise InvalidGuessedLetterException
-    if game['remaining_misses'] == 0 or game['masked_word'] == game['answer_word']:
+    if game['answer_word'] == game['masked_word'] or game['remaining_misses'] == 0:
         raise GameFinishedException
-    game['masked_word'] = _uncover_word(game['answer_word'], game['masked_word'], letter)
-    game['previous_guesses'].append(letter)
-    if letter not in game['answer_word']:
-        game['remaining_misses'] -= 1
-    if game['remaining_misses'] == 0:
-        raise GameLostException
-    if game['masked_word'] == game['answer_word']:
+    #letter not in answer
+    if game['masked_word'] == _uncover_word(word_to_guess, game['masked_word'], letter):
+        game['previous_guesses'].append(letter) #adding to guesses
+        game['remaining_misses'] -= 1 #remaining misses go down by one
+    #letter in answer
+    elif game['masked_word'] != _uncover_word(word_to_guess, game['masked_word'], letter):
+        game['previous_guesses'].append(letter) #adding to guesses
+        game['masked_word'] = _uncover_word(word_to_guess, game['masked_word'], letter) #updating masked word
+    if game['answer_word'] == game['masked_word']:
         raise GameWonException
+    if game['remaining_misses'] == 0:
+        game['masked_word'] = masked_word
+        raise GameLostException
+
 
 def start_new_game(list_of_words=None, number_of_guesses=5):
     if list_of_words is None:
